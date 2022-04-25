fa260532a5a75023acdbb797614470fe6349e78d
RJudge1170 <ryantj@live.unc.edu>
2022-04-25 01:21:36 -0400

    renamed accesslog both areas

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const database = require('better-sqlite3')
 
 const logdb = new database('log.db')
 
-const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and 'access';`)
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 let row = stmt.get();
 
 if(row === undefined) {
